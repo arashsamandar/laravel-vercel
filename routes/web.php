26b2908bee43ddf9ebc4b7af9fd4f4b6c7510bde
arashsamandar <arash.internet@gmail.com>
2024-07-14 21:37:15 +0330

    Testing Database On Supabase On Local Is Done

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test',function(){
+    return response()->json([
+        'status' => 'success',
+        'message' => 'hello arash'
+    ])->header('Content-Type','application/json');
+});
+
+Route::get('/testUser',function (){
+    return \App\Models\User::all();
+});
+
+Route::get('/testUser2',[\App\Http\Controllers\UsersController::class,'showUsers']);
