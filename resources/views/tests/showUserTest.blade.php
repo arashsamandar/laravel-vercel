c6ece21efe1661aa66643a0ae15831508fbf2d10
arashsamandar <arash.internet@gmail.com>
2024-07-15 19:48:56 +0330

    Commented The Favicon In Blade Views Out

--- resources/views/tests/showUserTest.blade.php
+++ resources/views/tests/showUserTest.blade.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
-    <link rel="icon" href="{{ asset('favicon/favicon.ico') }}" type="image/x-icon">
+{{--    <link rel="icon" href="{{ asset('favicon/favicon.ico') }}" type="image/x-icon">--}}
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <title>Document</title>
 </head>
