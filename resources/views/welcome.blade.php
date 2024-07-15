fdace7827585f78b518aceed1a4b1d790a0d8af0
arashsamandar <arash.internet@gmail.com>
2024-07-15 19:30:36 +0330

    <link href={{asset('favicon.ico')}} /> Was Added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,6 +7,7 @@
         <title>Laravel</title>
 
         <!-- Fonts -->
+        <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
