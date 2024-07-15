658ec6b57eaf23749597f95c88b87437b4eb2480
arashsamandar <arash.internet@gmail.com>
2024-07-15 15:13:50 +0330

    Tailwind Was Added & Compiled

--- resources/views/tests/showUserTest.blade.php
+++ resources/views/tests/showUserTest.blade.php
@@ -5,9 +5,10 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <title>Document</title>
 </head>
 <body>
-    <h1>Hello Arash -- Welcome To :  showUserTest.blade.php </h1>
+    <h1 class="text-3xl font-bold underline">Hello Arash -- Welcome To :  showUserTest.blade.php </h1>
 </body>
 </html>
